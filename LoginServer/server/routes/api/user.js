d33d78e1a92eb9cb019a7c0ba385ae32be215055
20171609 <clw3wb@naver.com>
2022-03-25 07:15:11 +0900

    전체 유저 정보 가져오는 router 삭제

--- LoginServer/server/routes/api/user.js
+++ LoginServer/server/routes/api/user.js
@@ -9,19 +9,6 @@ const User = require('../../models/user');
 
 const router = express.Router();
 
-// @routes Get all user
-router.get('/', async(req, res) =>{
-    try{
-        const users = await User.find();
-        if (!users) throw Error("No users");
-        res.status(200).json(users);
-
-    } catch (e) {
-        console.log(e);
-        res.status(400).json({msg: e.message});
-    }
-})
-
 router.get('/checked', async(req, res) =>{
     const {id, email, nickname} = req.body;
 
