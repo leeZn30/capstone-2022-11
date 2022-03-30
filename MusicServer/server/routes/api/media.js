cb4ff3b2a371a0eff5eaee8f443666e1cc3e0721
yeringii <yeringii@kookmin.ac.kr>
2022-03-30 15:08:01 +0900

    음악, 이미지 파일 업로드 기능 추가

--- MusicServer/server/routes/api/media.js
+++ MusicServer/server/routes/api/media.js
@@ -3,25 +3,31 @@ const multiparty = require('multiparty')
 const url = require('url')
 const config = require('../../config/index');
 const AWS = require('aws-sdk');
+const path = require("path");
 
+
+const {BUCKET_NAME} = config
 const {AWS_BUCKET_URL} = config
 const {AWS_BUCKET_ACCESS_ID} = config
 const {AWS_BUCKET_ACCESS_KEY} = config
 
 
-const BUCKET_NAME = 'bucket-bsn0zi'
+
 
 AWS.config.update({
     region:         'ap-northeast-2',
   accessKeyId:    AWS_BUCKET_ACCESS_ID,
   secretAccessKey: AWS_BUCKET_ACCESS_KEY
 })
+
 router.get('/*', (req,res)=>{
     const {pathname} = url.parse(req.url, true)
     res.redirect(`https://${AWS_BUCKET_URL}${pathname}`)
 })
 
 router.post('/',function(req, res){
+    //body 확인용
+    console.log(req.body);
     const form = new multiparty.Form()
     // 에러 처리
     form.on('error', function(err){
@@ -32,14 +38,24 @@ router.post('/',function(req, res){
         //파일 아니면 res.error
         if(!part.filename)
             return res.status(400).json({msg: "올바르지 않은 파일입니다."});
-        //filename -> id+totalNum db에서 가져와서
-        const filename = part.filename // 버킷에 올라갈 디렉토리+파일의 이름
-        const params = { Bucket:BUCKET_NAME, Key:filename, Body:part, ContentType: 'audio/mpeg' }
+        //filename -> id+totalNum
+        const filename = part.filename
+        //음악, 이미지 파일 다른 폴더로 업로드
+        const extension = path.extname(part.filename);
+        var params = {}
+        //파일 확장자 확인
+        if(extension === '.mp3' || extension === '.wav')
+            params = { Bucket:BUCKET_NAME, Key:'Music/'+filename, Body:part, ContentType: 'audio/mpeg' }
+        else if(extension === '.jpg' || extension === '.png')
+            params = { Bucket:BUCKET_NAME, Key:'Image/'+filename, Body:part, ContentType: 'image' }
+        else
+            return res.status(400).json({msg: "올바르지 않은 파일입니다."});
         const upload = new AWS.S3.ManagedUpload({ params });
         upload.promise()
 
         part.on('end', function(){
             // 파일 업로드 후 실행할 추가 코드
+            console.log("Uploaded!")
         })
         part.on('error', function(err){
             console.log(err)
@@ -51,6 +67,7 @@ router.post('/',function(req, res){
         res.end()
     })
     form.parse(req)
+    
 })
 
 module.exports = router
