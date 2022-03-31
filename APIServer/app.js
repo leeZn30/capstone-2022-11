ccd5425d065c00f5766df9515d0c610f3215e6cb
20171609 <clw3wb@naver.com>
2022-03-31 15:02:43 +0900

    LoginServer와 MusicServer 통합

--- APIServer/app.js
+++ APIServer/app.js
@@ -9,6 +9,8 @@ const config = require('./config/index');
 
 const usersRouters = require('./routes/api/user');
 const authRouters = require('./routes/api/auth');
+const mediaRouter = require('./routes/api/media');
+const musicRouter = require('./routes/api/musicDB');
 
 const app = express();
 const { MONGO_URI } = config;
@@ -23,15 +25,37 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-    })
+    useNewUrlParser: true,
+})
     .then(() => console.log("MongoDB connecting Success!!"))
     .catch((e) => console.log(e));
 
 //routes
 app.get('/');
 
+app.get('/upload',function(req, res){
+    const body = `
+<html>
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body>
+      <form action="/api/media" enctype="multipart/form-data" method="post">
+          <input type="file" name="file1" multiple="multiple">
+          <input type="file" name="file2" multiple="multiple">
+          <input type="submit" value="Upload file" />
+      </form>
+  </body>
+</html>
+`
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(body);
+    res.end();
+})
+
 app.use('/api/user', usersRouters);
 app.use('/api/auth', authRouters);
+app.use('/api/media', mediaRouter);
+app.use('/api/music', musicRouter);
 
 module.exports = app;
--- APIServer/config/index.js
+++ APIServer/config/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const obj = {};
 obj.MONGO_URI = process.env.MONGO_URI;
+obj.JWT_SECRET = process.env.JWT_SECRET;
 obj.PORT = process.env.PORT;
 obj.BUCKET_NAME = process.env.BUCKET_NAME;
 obj.AWS_BUCKET_URL = process.env.AWS_BUCKET_URL;
