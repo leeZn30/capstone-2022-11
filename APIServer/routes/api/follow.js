9198d79409afcc3d6085e2c2850e7170149da298
20171609 <clw3wb@naver.com>
2022-05-08 01:59:44 +0900

    팔로우 response 및 팔로우 중복 유저 처리

--- APIServer/routes/api/follow.js
+++ APIServer/routes/api/follow.js
@@ -25,11 +25,29 @@ router.post('/', auth, async(req, res)=>{
     const {userId, userNickname} = req.body;
     const id = req.user.id;
     const nickname = req.user.nickname;
+    let userList = [];
+    let duplicate = false;
 
-    await User.updateOne({id: id}, {$addToSet: { follow: [userId, userNickname]}});
-    await User.updateOne({id: userId}, {$addToSet: { follower: [id, nickname]}});
-    res.status(200).json({message:"OK"})
+    await User.findOne({id:id}).then((user)=>{
+        userList = user.follow;
+    })
 
+    for (let i = 0; i < userList.length; i++){
+        if (userList[i][0] === userId) {
+            duplicate = true;
+        }
+    }
+
+    if (duplicate) {
+        res.status(450).json({msg:"already exist"})
+    }
+    else {
+        await User.updateOne({id: id}, {$addToSet: { follow: [userId, userNickname]}});
+        await User.updateOne({id: userId}, {$addToSet: { follower: [id, nickname]}});
+        User.findOne({id:id}).then((user)=> {
+            res.status(200).json({follow:user.follow})
+        })
+    }
 })
 
 router.post('/delete', auth, async(req, res)=>{
@@ -39,7 +57,9 @@ router.post('/delete', auth, async(req, res)=>{
 
     await User.updateOne({id: id}, {$pull: { follow: [userId, userNickname]}});
     await User.updateOne({id: userId}, {$pull: { follower: [id, nickname]}});
-    res.status(200).json({message:"OK"})
+    User.findOne({id:id}).then((user)=> {
+        res.status(200).json({follow:user.follow})
+    })
 
 })
 
