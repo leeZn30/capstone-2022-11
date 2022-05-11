e0e4ab4e602ee505511296fbdd5bcb3ec03a684e
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2022-05-12 02:59:49 +0900

    작은 화면 스케일 업 가능

--- unity/Assets/Scripts/03_Park/UI_script/BigVideoPanel.cs
+++ unity/Assets/Scripts/03_Park/UI_script/BigVideoPanel.cs
@@ -6,17 +6,24 @@ using UnityEngine.UI;
 public class BigVideoPanel : MonoBehaviour
 {
     [SerializeField] private GameObject shutButton;
+    [SerializeField] private SmallVideoPanel smallVideoPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        shutButton.GetComponent<Button>().onClick.AddListener(ScaleDownPanel);
+        shutButton.GetComponent<Button>().onClick.AddListener(smallVideoPanel.ScaleDownPanle);
     }
 
     private void OnEnable()
     {
         GameManager.instance.myPlayer.GetComponent<PlayerControl>().isMoveAble = false;
         GameManager.instance.myPlayer.GetComponent<PlayerControl>().isUIActable = false;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.myPlayer.GetComponent<PlayerControl>().isMoveAble = true;
+        GameManager.instance.myPlayer.GetComponent<PlayerControl>().isUIActable = true;
     }
 
 
--- unity/Assets/Scripts/03_Park/UI_script/SmallVideoPanel.cs
+++ unity/Assets/Scripts/03_Park/UI_script/SmallVideoPanel.cs
@@ -6,11 +6,51 @@ using Photon.Pun;
 
 public class SmallVideoPanel : MonoBehaviour
 {
+    public Vector3 initPosition;
+    public Vector3 initSize;
+
+    private void Start()
+    {
+        initPosition = transform.localPosition;
+        initSize = transform.localScale;
+    }
+
 
     public void ScaleUpPanel()
     {
-        this.gameObject.SetActive(false);
+        StartCoroutine(ScaleUp(this.transform, new Vector3(-2, -2, 0), 2f));
         FindObjectOfType<Canvas>().transform.Find("bigVideoPanel").gameObject.SetActive(true);
+        this.gameObject.GetComponent<Button>().enabled = false;
     }
-    
+
+    public void ScaleDownPanle()
+    {
+        StartCoroutine(ScaleDown(this.transform, 2f));
+        FindObjectOfType<Canvas>().transform.Find("bigVideoPanel").gameObject.SetActive(false);
+        this.gameObject.GetComponent<Button>().enabled = true;
+    }
+
+    IEnumerator ScaleUp(Transform transform, Vector3 upScale, float duration)
+    {
+        Vector3 initialScale = transform.localScale;
+
+        for (float time = 0; time < duration; time += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(initialScale, upScale, duration);
+            transform.localPosition = new Vector3(-230, 0, 0);
+            yield return null;
+        }
+    }
+    IEnumerator ScaleDown(Transform transform, float duration)
+    {
+        Vector3 initialScale = transform.localScale;
+
+        for (float time = 0; time < duration; time += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(initialScale, initSize, duration);
+            transform.localPosition = initPosition;
+            yield return null;
+        }
+    }
+
 }
