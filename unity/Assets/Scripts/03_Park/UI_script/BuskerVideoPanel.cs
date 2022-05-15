798ab90e4dc78e430a847db62040c7127442b5ba
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2022-05-16 00:41:51 +0900

    카메라 및 마이크 체크 추가

--- unity/Assets/Scripts/03_Park/UI_script/BuskerVideoPanel.cs
+++ unity/Assets/Scripts/03_Park/UI_script/BuskerVideoPanel.cs
@@ -30,10 +30,6 @@ public class BuskerVideoPanel : MonoBehaviour
     // Input Field
     [SerializeField] private TMP_InputField titleInput;
 
-    // 카메라, 마이크
-    [SerializeField] private RawImage cameraImage;
-    [SerializeField] private AudioSource MicSource;
-
     // small Video Panel
     [SerializeField] private GameObject smallVideo;
 
@@ -46,18 +42,87 @@ public class BuskerVideoPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (isCameraOn)
             CameraCheck.color = Color.green;
         else
-            CameraCheck.color = Color.white;
+            CameraCheck.color = Color.red;
 
         if (isMicOn)
             MicCheck.color = Color.green;
         else
-            MicCheck.color = Color.white;
-
+            MicCheck.color = Color.red;
     }
+
+    private void cameraConnect()
+    {
+
+        // 카메라
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        int selectedCameraIndex = -1;
+        for (int i = 0; i < devices.Length; i++)
+        {
+            // 사용 가능한 카메라 로그
+            Debug.Log("Available Webcam: " + devices[i].name + ((devices[i].isFrontFacing) ? "(Front)" : "(Back)"));
+
+            // 후면 카메라인지 체크
+            if (devices[i].isFrontFacing)
+            {
+                // 해당 카메라 선택
+                selectedCameraIndex = i;
+                break;
+            }
+        }
+
+        // WebCamTexture 생성
+        if (selectedCameraIndex >= 0)
+        {
+            // 선택된 카메라에 대한 새로운 WebCamTexture를 생성
+            textureWebCam = new WebCamTexture(devices[selectedCameraIndex].name);
+
+            // 원하는 FPS를 설정
+            if (textureWebCam != null)
+            {
+                textureWebCam.requestedFPS = 60;
+            }
+        }
+
+        // objectTarget으로 카메라가 표시되도록 설정
+        if (textureWebCam != null)
+        {
+            // 카메라
+            objectTarget.GetComponent<RawImage>().texture = textureWebCam;
+            textureWebCam.Play();
+
+            // 카메라 켜졌다고 표시
+            isCameraOn = true;
+
+        }
+        else // 카메라 텍스쳐 없음
+        {
+            Debug.Log("No Camera Texture");
+        }
+    }
+
+    private void micConnect()
+    {
+        try
+        {
+            string mic = Microphone.devices[0];
+            micAudioSource.clip = Microphone.Start(mic, true, 10, 44100);
+            while (!(Microphone.GetPosition(mic) > 0)) { } // Wait until the recording has started
+            micAudioSource.Play(); // Play the audio source!
+
+            // 마이크 켜졌다고 표시
+            isMicOn = true;
+
+        }
+        catch
+        {
+            Debug.Log("No mic");
+        }
+    }
+
 
     private void exitPanel()
     {
@@ -68,8 +133,15 @@ public class BuskerVideoPanel : MonoBehaviour
 
     public void setDevice()
     {
-        StartButton.onClick.RemoveAllListeners(); // 지워주고 해야함
-        StartButton.onClick.AddListener(StartBusking);
+        cameraConnect();
+        micConnect();
+
+        if (isMicOn && isCameraOn)
+        {
+            StartButton.onClick.RemoveAllListeners(); // 지워주고 해야함
+            StartButton.onClick.AddListener(StartBusking);
+        }
+
     }
 
     // 버스킹 인터렉티브
