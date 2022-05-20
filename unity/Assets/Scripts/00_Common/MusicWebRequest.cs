d8546c5de8ba3bd3b982edbb27cd25bd03e90347
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2022-05-20 14:52:29 +0900

    이미지, 폴더 구조 변경, 팔로우 버그 수정

--- unity/Assets/Scripts/00_Common/MusicWebRequest.cs
+++ unity/Assets/Scripts/00_Common/MusicWebRequest.cs
@@ -5,193 +5,9 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Networking;
 using LitJson;
-using NAudio;
-using NAudio.Wave;
 using Cysharp.Threading.Tasks;
 using System;
-//using System.IO;
-//using System;
-/*
-public class PcmHeader
-{
-    #region Public types & data
 
-    public int BitDepth { get; }
-    public int AudioSampleSize { get; }
-    public int AudioSampleCount { get; }
-    public ushort Channels { get; }
-    public int SampleRate { get; }
-    public int AudioStartIndex { get; }
-    public int ByteRate { get; }
-    public ushort BlockAlign { get; }
-
-    #endregion
-
-    #region Constructors & Finalizer
-
-    private PcmHeader(int bitDepth,
-        int audioSize,
-        int audioStartIndex,
-        ushort channels,
-        int sampleRate,
-        int byteRate,
-        ushort blockAlign)
-    {
-        BitDepth = bitDepth;
-        _negativeDepth = Mathf.Pow(2f, BitDepth - 1f);
-        _positiveDepth = _negativeDepth - 1f;
-
-        AudioSampleSize = bitDepth / 8;
-        AudioSampleCount = Mathf.FloorToInt(audioSize / (float)AudioSampleSize);
-        AudioStartIndex = audioStartIndex;
-
-        Channels = channels;
-        SampleRate = sampleRate;
-        ByteRate = byteRate;
-        BlockAlign = blockAlign;
-    }
-
-    #endregion
-
-    #region Public Methods
-
-    public static PcmHeader FromBytes(byte[] pcmBytes)
-    {
-        using var memoryStream = new MemoryStream(pcmBytes);
-        return FromStream(memoryStream);
-    }
-
-    public static PcmHeader FromStream(Stream pcmStream)
-    {
-        pcmStream.Position = SizeIndex;
-        using BinaryReader reader = new BinaryReader(pcmStream);
-
-        int headerSize = reader.ReadInt32();  // 16
-        ushort audioFormatCode = reader.ReadUInt16(); // 20
-
-        string audioFormat = GetAudioFormatFromCode(audioFormatCode);
-        if (audioFormatCode != 1 && audioFormatCode == 65534)
-        {
-            // Only uncompressed PCM wav files are supported.
-            throw new ArgumentOutOfRangeException(nameof(pcmStream),pcmStream,
-                                                  $"Detected format code '{audioFormatCode}' {audioFormat}, but only PCM and WaveFormatExtensible uncompressed formats are currently supported.");
-        }
-
-        ushort channelCount = reader.ReadUInt16(); // 22
-        int sampleRate = reader.ReadInt32();  // 24
-        int byteRate = reader.ReadInt32();  // 28
-        ushort blockAlign = reader.ReadUInt16(); // 32
-        ushort bitDepth = reader.ReadUInt16(); //34
-
-        pcmStream.Position = SizeIndex + headerSize + 2 * sizeof(int); // Header end index
-        int audioSize = reader.ReadInt32();                            // Audio size index
-
-        return new PcmHeader(bitDepth, audioSize, (int)pcmStream.Position, channelCount, sampleRate, byteRate, blockAlign); // audio start index
-    }
-
-    public float NormalizeSample(float rawSample)
-    {
-        float sampleDepth = rawSample < 0 ? _negativeDepth : _positiveDepth;
-        return rawSample / sampleDepth;
-    }
-
-    #endregion
-
-    #region Private Methods
-
-    private static string GetAudioFormatFromCode(ushort code)
-    {
-        switch (code)
-        {
-            case 1: return "PCM";
-            case 2: return "ADPCM";
-            case 3: return "IEEE";
-            case 7: return "?-law";
-            case 65534: return "WaveFormatExtensible";
-            default: throw new ArgumentOutOfRangeException(nameof(code), code, "Unknown wav code format.");
-        }
-    }
-
-    #endregion
-
-    #region Private types & Data
-
-    private const int SizeIndex = 16;
-
-    private readonly float _positiveDepth;
-    private readonly float _negativeDepth;
-
-    #endregion
-}
-public struct PcmData
-{
-    #region Public types & data
-
-    public float[] Value { get; }
-    public int Length { get; }
-    public int Channels { get; }
-    public int SampleRate { get; }
-
-    #endregion
-
-    #region Constructors & Finalizer
-
-    private PcmData(float[] value, int channels, int sampleRate)
-    {
-        Value = value;
-        Length = value.Length;
-        Channels = channels;
-        SampleRate = sampleRate;
-    }
-
-    #endregion
-
-    #region Public Methods
-
-    public static PcmData FromBytes(byte[] bytes)
-    {
-        if (bytes == null)
-        {
-            throw new ArgumentNullException(nameof(bytes));
-        }
-
-        PcmHeader pcmHeader = PcmHeader.FromBytes(bytes);
-        if (pcmHeader.BitDepth != 16 && pcmHeader.BitDepth != 32 && pcmHeader.BitDepth != 8)
-        {
-            throw new ArgumentOutOfRangeException(nameof(pcmHeader.BitDepth), pcmHeader.BitDepth, "Supported values are: 8, 16, 32");
-        }
-
-        float[] samples = new float[pcmHeader.AudioSampleCount];
-        for (int i = 0; i < samples.Length; ++i)
-        {
-            int byteIndex = pcmHeader.AudioStartIndex + i * pcmHeader.AudioSampleSize;
-            float rawSample;
-            switch (pcmHeader.BitDepth)
-            {
-                case 8:
-                    rawSample = bytes[byteIndex];
-                    break;
-
-                case 16:
-                    rawSample = BitConverter.ToInt16(bytes, byteIndex);
-                    break;
-
-                case 32:
-                    rawSample = BitConverter.ToInt32(bytes, byteIndex);
-                    break;
-
-                default: throw new ArgumentOutOfRangeException(nameof(pcmHeader.BitDepth), pcmHeader.BitDepth, "Supported values are: 8, 16, 32");
-            }
-
-            samples[i] = pcmHeader.NormalizeSample(rawSample); // normalize sample between [-1f, 1f]
-        }
-
-        return new PcmData(samples, pcmHeader.Channels, pcmHeader.SampleRate);
-    }
-
-    #endregion
-}
-*/
 public class AudioClipPlay
 {
     public AudioClip audioClip;
@@ -642,12 +458,12 @@ public class MusicWebRequest : MonoBehaviour
             if (e.ResponseCode == 400)
             {
                 Debug.Log(e.ResponseCode+" [GET_MusicListAsync] 토큰 만료");
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -714,13 +530,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode+"[GetAudioClipAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -823,13 +639,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode+" [GET_SearchMusicTitleAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -907,13 +723,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode+"[GET_SpecificMusicListAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -984,13 +800,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             { 
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[GET_FollowSystemUserListAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1071,13 +887,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[ GET_SearchUserAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1091,7 +907,7 @@ public class MusicWebRequest : MonoBehaviour
             return null;
         }
     }
-    protected async UniTask POST_FollowUserAsync(string userID,string userName, bool isDelete=false)
+    protected async UniTask<StringList> POST_FollowUserAsync(string userID,string userName, bool isDelete=false)
     {
         try
         {
@@ -1105,7 +921,7 @@ public class MusicWebRequest : MonoBehaviour
 
             Debug.Log("follow "+isDelete+ " json: " + json);
 
-            using (UnityWebRequest www = UnityWebRequest.Post(url + "/api/follow" + (isDelete? "/api/delete" : ""),json))
+            using (UnityWebRequest www = UnityWebRequest.Post(url + "/api/follow" + (isDelete? "/delete" : ""),json))
             {
 
                 www.SetRequestHeader("token", UserData.Instance.Token);
@@ -1114,29 +930,39 @@ public class MusicWebRequest : MonoBehaviour
                 www.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json));
 
                 await www.SendWebRequest();
-
+                StringList sl = new StringList();
                 if (www.error == null)
                 {
-
-                    Debug.Log("done");
+                    List<String> strList = new List<string>();
+                    Debug.Log("done"); 
+                    string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                    JsonData jsonData = JsonToObject(jsonResult)["follow"];
+                    Debug.Log(jsonResult);
+                    foreach (JsonData j in jsonData)
+                    {
+                        strList.Add((string)(j[0]));
+                    }
+                    sl.stringList = strList;
+                    return sl;
 
                 }
                 else
                 {
                     Debug.Log(www.error.ToString());
-
+                    return null;
                 }
             }
         }
         catch (ArgumentNullException e)
         {
             Debug.Log("follow 요청 취소됨");
+            return null;
         }
         catch (UnityWebRequestException e)
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_FollowUserAsync] 토큰 만료");
             }
             else if (e.ResponseCode == 450)
@@ -1147,16 +973,18 @@ public class MusicWebRequest : MonoBehaviour
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
                 Debug.Log(e);
             }
+            return null;
         }
         catch (Exception e)
         {
             Debug.LogError(e);
+            return null;
         }
     }
     protected async UniTask<User> GET_UserInfoAsync(string userId)
@@ -1227,14 +1055,14 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[ GET_UserInfoAsync] 토큰 만료");
 
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1289,13 +1117,13 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_AddPlayCount] 토큰 만료");
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1354,14 +1182,14 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_MakeListAsync] 토큰 만료");
 
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1420,14 +1248,14 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_DeleteListAsync] 토큰 만료");
 
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1483,14 +1311,14 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[GET_ListNameAsync] 토큰 만료");
 
             }
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1561,7 +1389,7 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_DeleteListAsync] 토큰 만료");
 
             }
@@ -1573,7 +1401,7 @@ public class MusicWebRequest : MonoBehaviour
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
@@ -1621,7 +1449,7 @@ public class MusicWebRequest : MonoBehaviour
         {
             if (e.ResponseCode == 400 || e.ResponseCode == 401)
             {
-                Popup.Instance.Open(0);
+                ErrorPopup.Instance.Open(0);
                 Debug.Log(e.ResponseCode + "[POST_DeleteListAsync] 토큰 만료");
 
             }
@@ -1633,7 +1461,7 @@ public class MusicWebRequest : MonoBehaviour
             else if (e.ResponseCode == 0)
             {
                 Debug.Log("서버와 연결 끊어짐");
-                Popup.Instance.Open(1);
+                ErrorPopup.Instance.Open(1);
             }
             else
             {
